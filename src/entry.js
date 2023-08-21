eef4bef8e0bcf55690cc38a065c1621ba5027315
Allan Victor Lima e Pires <allan-pires@github.com>
2023-08-21 18:09:19 +0000

    Throw error to test catch block

--- src/entry.js
+++ src/entry.js
@@ -3,9 +3,11 @@ const { exportSecrets } = require('./action');
 
 (async () => {
     try {
+        throw new Error(`failed to retrieve vault token.`);
         await core.group('Get Vault Secrets', exportSecrets);
     } catch (error) {
         core.setFailed(error?.message || "Get Vault Secrets failed");
-        throw error
+        console.log(error);
+        throw error;
     }
 })();
